f2b6982663dd0ea08fa56e18087beb96621e151e
Billy Criddle <billycrid@hotmail.co.uk>
2025-04-15 11:18:28 +0100

    added markdown support

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactMarkdown from 'react-markdown';
 
 export const Posts = () => {
     const [posts, setPosts] = useState([]);
@@ -28,7 +29,7 @@ export const Posts = () => {
             </div>
             <br/>
             <div style={{minHeight: '60px', color: 'black'}}>
-                {post.intro}
+                <ReactMarkdown>{post.intro}</ReactMarkdown>
             </div>
           </div>
         ))}
--- src/content/home-posts/map-headline-billy-post-intro-hello-n-cta_link-https-google-com.md
+++
@@ -1,6 +0,0 @@
----
-headline: Billy Post
-intro: |
-  Hello
-cta_link: https://google.com
----
